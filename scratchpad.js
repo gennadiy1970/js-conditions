b2996754176a95ddfbd955942cdf7c8a4300dcf1
gennadiy1970 <ggm_kharkiv@ukr.net>
2018-08-23 16:38:21 +0300

    update view

--- scratchpad.js
+++ scratchpad.js
@@ -1,5 +1,16 @@
 {
 
+undefined == null  // true
+null == null   // true
+undefined == undefined // true
+
+undefined === null  // false
+null === null   // true
+undefined === undefined // true
+  
+undefined == 0  // false
+undefined == false  // false
+  
 0 && 0 /* 0 */
 0 && 1 /* 0 */
 1 && 0 /* 0 */
@@ -7,7 +18,7 @@
 
 0 || 0 /* 0 */
 0 || 1 /* 0 */
-1 || 0 /* 0 */
+1 || 1 /* 0 */
 1 || 1 /* 1 */
 
 !1 /* false */
@@ -33,16 +44,18 @@ left   /*  5  */
 left =  0 || 0 || 0|| 0 || 0 || 0;
 left   /*  0  */
 
-0 === '' 
+
+0 == ''   /* true */
+0 === '' /*  false */
 
   
-  let a = 4;  /* 100 */
-  let b = a << 2; /* 100 00 или 16 */
+  let a =  0b100;  /* 0b100 или 4 */
+  let b = a << 2; /* 0b10000 или 16 */
  
-  let c = 1024; /* 10 000 000 000 */
-  let d = c >> 8; /* 100 */
+  let c = 1024; /* 0b10000000000 */
+  let d = c >> 8; /* 0b100 */
   
-  let e = a & b; 
+  let e = a & b;  /* 0 */ 
   /* &
   00 100  
   10 000 
@@ -50,7 +63,7 @@ left   /*  0  */
   00 000 
   */
   
-  let f = a | b; 
+  let f = a | b; /* 0b10100  */
   /* |
   00 100  
   10 000 
@@ -61,20 +74,13 @@ left   /*  0  */
   let word =  12345;  //  0b011000000111001; 
   let key = 40831;    // 0b1001111101111111;  
   let cript = word ^ key; 
-  /*  ^
+  /*  
               0b011000000111001
+              ^
              0b1001111101111111
   44870  или 0b1010111101000110  
   */
   let uncript = cript ^ key;  /* 12345 */
 
-  undefined == null  // true
-  null == null   // true
-  undefined == undefined // true
-  undefined === null  // false
-  null === null   // true
-  undefined === undefined // true
-  undefined == 0  // false
-  undefined == false  // false
 }
 
